982d1a467d71d3407c12289c98c5ea8558f741e6
eujev <87488490+eujev@users.noreply.github.com>
2025-06-03 19:25:59 +0200

    Setup Flask project

---
+++ app.py
@@ -0,0 +1,19 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
